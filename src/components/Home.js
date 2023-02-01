e4e1e941c607a3a9148335999cef718e9df8e82d
simmeringstew <dev.95a2e@simplelogin.com>
2023-02-01 10:41:09 -0500

    Did styling for sorting elements

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,8 @@ const Home = () => {
 
     const [countries, setCountries] = useState([...data]);
     const [search, setSearch] = useState("");
+    const [option, setOption] = useState("default");
+
     const nameSort = (value) => {
         if (value === "") {
             setCountries([...data]);
@@ -18,9 +20,13 @@ const Home = () => {
         setSearch(value);
     }
 
+    const filter = (value) => {
+
+    }
+
     return (
         <div className="home">
-        <Sort search={search} nameSort={nameSort} />
+        <Sort search={search} nameSort={nameSort} filter={filter} />
             <div className="cards container">
             {countries.map(country =>
             <CountryCard key={country.numericCode} country={country} />
--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,12 +1,31 @@
-const Sort = ({ search, nameSort }) => {
+import "../styles/Sort.css";
 
-    const handleChange = (e) => {
+const Sort = ({ search, nameSort, filter }) => {
+
+    const handleType = (e) => {
         nameSort(e.target.value);
     }
 
+    const handleChange = (e) => {
+        filter(e.target.value);
+    }
+
     return(
-        <div className="sort">
-            <input value={search} placeholder="Search for a country..." onChange={e => handleChange(e)} />
+        <div className="sort container">
+            <div className="search-box">
+                <input value={search} placeholder="Search for a country..." onChange={e => handleType(e)} />
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                </svg>
+            </div>
+            <select className="select" onChange={e => handleChange(e)}>
+                <option value="default">Filter by Region</option>
+                <option value="africa">Africa</option>
+                <option value="americas">Americas</option>
+                <option value="asia">Asia</option>
+                <option value="europe">Europe</option>
+                <option value="oceania">Oceania</option>
+            </select>
         </div>
     );
 }
