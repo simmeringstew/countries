d9eabeb3ad336d3436824686149637ff13449931
simmeringstew <dev.95a2e@simplelogin.com>
2023-02-01 12:20:20 -0500

    Added link to cards

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,16 +1,24 @@
+import { Link } from "react-router-dom";
 import "../styles/CountryCard.css";
 
 const CountryCard = ({ country }) => {
+
+    const handleClick = () => {
+
+    }
+
     return (
-        <div className="card">
-            <img src={country.flags.svg} alt={country.name} />
-            <h2>{country.name}</h2>
-            <ul>
-                <li><span className="bold">Population:</span> {country.population}</li>
-                <li><span className="bold">Region:</span> {country.region}</li>
-                <li><span className="bold">Capital:</span> {country.capital}</li>
-            </ul>
-        </div>
+        <Link to={`/${country.name}`} onClick={handleClick} className="card-link">
+            <div className="card">
+                <img src={country.flags.svg} alt={country.name} />
+                <h2>{country.name}</h2>
+                <ul>
+                    <li><span className="bold">Population:</span> {country.population}</li>
+                    <li><span className="bold">Region:</span> {country.region}</li>
+                    <li><span className="bold">Capital:</span> {country.capital}</li>
+                </ul>
+            </div>
+        </Link>
     );
 }
 
